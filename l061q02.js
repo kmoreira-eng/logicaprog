df8f0e854b2508d72aaaa90e67c83f0738ee95ba
Kauany Vitoria Lima Moreira <eng.kmoreira@gmail.com>
2026-06-17 11:53:25 -0300

    Aula de js - 17/06

---
+++ l061q02.js
@@ -0,0 +1,15 @@
+/*Desenvolver um programa que pergunte o sexo da pessoa e dê como resposta a seguinte orientação de acordo
+com o sexo informado: “Banheiro masculino à direita” ou “Banheiro feminino à esquerda”.*/
+
+let sexof, sexom;
+
+console.log("Informe o seu sexo: ");
+sexof = "Feminino";
+sexom = "Masculino";
+
+console.log(`Seu sexo é: ${sexofm}`);
+
+switch (sexof) {
+    case sexof:
+        console.log(
+}
