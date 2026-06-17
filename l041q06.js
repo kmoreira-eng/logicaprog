3144dbb1eab0945e8b4a7b3d56d8b191f5aa0fca
Kauany Vitoria Lima Moreira <eng.kmoreira@gmail.com>
2026-06-17 10:46:23 -0300

    alteracao l041q06.js

--- l041q06.js
+++ l041q06.js
@@ -1,1 +1,20 @@
 //comentário inserido em 17/06/2004
+//Desenvolver um programa que pergunte dois valores
+//numéricos inteiros e apresente o valor da diferença
+//entre o maior valor e o menor valor lido.
+
+let val1, val2;
+let dif;
+
+console.log("Insira o Valor 1");
+val1 = 10;
+console.log(`${val1}`);
+console.log("Insira o Valor 2");
+val2 = 7;
+console.log(`${val2}`);
+
+dif = val1 - val2;
+
+console.log(`A diferença é ${dif}`);
+
+console.log("FIM DO PROGRAMA");
