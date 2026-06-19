a174468fd3ed469efb98532ba8a2684d256c0472
Kauany Vitoria Lima Moreira <eng.kmoreira@gmail.com>
2026-06-19 09:38:38 -0300

    aula for - 19/06/2004

---
+++ l053q01.js
@@ -0,0 +1,7 @@
+// Desenvolver um programa que apresente todos os valores numéricos 
+// inteiros ímpares situados na faixa de 1000 a 1500.
+
+for ( let cont = 1000 ; cont <= 1500 ; cont++ ) {
+    if (cont)
+    console.log( cont );
+}
