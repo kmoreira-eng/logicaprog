46ead10aad1e3f0b6bef144c99283c037fb3e2f8
Kauany Vitoria Lima Moreira <eng.kmoreira@gmail.com>
2026-06-26 08:55:36 -0300

    revisaoprova03

---
+++ revisaoprova2.js
@@ -0,0 +1,14 @@
+//Crie 2 vetores chamados nome e profissao, cada um com 5 elementos.
+//Pergunte ao usuário o nome e a profissão
+//de 5 pessoas e preencha estes vetores. Ao final exiba
+// um relatório apontando o nome e a profissão
+// de cada uma das 5 pessoas cadastradas.
+
+console.log(`Informe um nome: `)
+
+let names = ["Kassya", "Tayssa, MariaDudu, Cawã, Nicole" ];
+let profissional = ["IA, Kpopper, Psicóloga, Estelionatário, Gari "];
+
+for (let cont = 0; < 4; cont++){
+
+}
